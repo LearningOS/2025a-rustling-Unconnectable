033b27e087fab9d5e391072500e8cc3ac428f357
Phrink <scpr7@outlook.com>
2025-09-17 10:33:51 +0800

    test1

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,32 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.is_empty() {
+            // 使用 is_empty() 更符合 Rust 习惯
+            return Person::default();
+        } else {
+            let vec_: Vec<&str> = s.split(",").collect();
+            if vec_.len() != 2 || vec_[0].is_empty() {
+                return Person::default();
+            } else {
+                let name_ = vec_[0];
+                let age_ = vec_[1];
+                let age_number = match age_.parse::<usize>() {
+                    Ok(age_number) => age_number,
+                    Err(_) => {
+                        return Person::default();
+                    }
+                };
+
+                Person {
+                    name: name_.to_string(),
+                    age: age_number,
+                }
+            }
+        }
     }
 }
 
@@ -138,3 +160,21 @@ mod tests {
         assert_eq!(p.age, 30);
     }
 }
+
+// From 特性用于值到值的转换.如果 From 针对某个类型被正确实现,那么 Into 特性应该反向生效.
+// 你可以在 https://doc.rust-lang.org/std/convert/trait.From.html 阅读更多相关内容.
+// 我们实现了 Default 特性,以便在提供的字符串无法转换为 Person 对象时作为备用选项.
+
+// 你的任务是完成这个实现,使得代码行 `let p = Person::from("Mark,20")` 能够编译通过.
+// 请注意,你需要将年龄部分解析为 `usize`,可以使用类似 `"4".parse::<usize>()` 的方法.
+// 需要适当地处理解析的结果.
+
+//
+// 步骤：
+// 1. 如果提供的字符串长度为 0,则返回 Person 的默认值.
+// 2. 将给定的字符串按逗号分隔.
+// 3. 从分隔结果中提取第一个元素作为姓名.
+// 4. 如果姓名为空,则返回 Person 的默认值.
+// 5. 从分隔结果中提取另一个元素并将其解析为 `usize` 作为年龄.
+//    如果解析年龄时出现问题,则返回 Person 的默认值.
+//    否则,返回一个使用解析结果实例化的 Person 对象.
--- exercises/tests/tests5.rs
+++ exercises/tests/tests5.rs
@@ -20,19 +20,18 @@
 // your own code, take a step back and use safe code instead!
 //
 // Execute `rustlings hint tests5` or use the `hint` watch subcommand for a
-// hint.
-
-// I AM NOT DONE
 
 /// # Safety
 ///
 /// The `address` must contain a mutable reference to a valid `u32` value.
-unsafe fn modify_by_address(address: usize) {
+unsafe fn modify_by_address(mut address: usize) {
     // TODO: Fill your safety notice of the code block below to match your
     // code's behavior and the contract of this function. You may use the
     // comment of the test below as your format reference.
     unsafe {
-        todo!("Your code goes here")
+        //todo!("Your code goes here")
+        let ptr_ = address as *mut u32;
+        *ptr_ = 0xaabbccdd;
     }
 }
 
@@ -45,7 +44,21 @@ mod tests {
         let mut t: u32 = 0x12345678;
         // SAFETY: The address is guaranteed to be valid and contains
         // a unique reference to a `u32` local variable.
-        unsafe { modify_by_address(&mut t as *mut u32 as usize) };
-        assert!(t == 0xAABBCCDD);
+        unsafe {
+            modify_by_address(&mut t as *mut u32 as usize);
+        }
+        assert!(t == 0xaabbccdd);
     }
 }
+// 当在项声明上标记 `unsafe`，例如函数、trait 等时，它同时声明了一个契约。
+// 然而，这个契约的内容无法仅通过单个关键字来表达。
+// 因此，你有责任在项的文档注释中的 `# Safety` 部分手动说明其内容。
+//
+// 当在由花括号包围的代码块上标记 `unsafe` 时，
+// 它声明了对某个契约的遵守，例如某个指针参数的有效性、某个内存地址的所有权。
+// 然而，如上文所述，你仍然需要在代码块的注释中说明如何遵守该契约。
+//
+// 注意：所有注释都是为了代码的可读性和可维护性，
+// 而 Rust 编译器将对你代码的正确性信任交给了你自己！
+// 如果你无法证明自己代码的内存安全性和正确性，
+// 请退一步，转而使用安全的代码！
