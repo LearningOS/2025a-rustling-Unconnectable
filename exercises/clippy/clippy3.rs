c25f232ebce51e4c34c9ab969f4d30a4af128844
Phrink <scpr7@outlook.com>
2025-09-17 17:55:11 +0800

    还差algo9 10 clippy3

--- exercises/clippy/clippy3.rs
+++ exercises/clippy/clippy3.rs
@@ -9,12 +9,12 @@ use std::mem::swap;
 fn main() {
     let my_option: Option<()> = None;
     if my_option.is_none() {
-       //my_option.unwrap();
+        //my_option.unwrap();
     }
 
     let my_arr = &[-1, -2, -3, -4, -5, -6];
     println!("My array! Here it is: {:?}", my_arr);
-    
+
     //vec![1, 2, 3, 4, 5].resize(0, 5);
     let my_empty_vec = vec![1, 2, 3, 4, 5];
     println!("This Vec is empty, see? {:?}", my_empty_vec);
---
+++ test.rs
@@ -0,0 +1,25 @@
+fn bracket_match(bracket: &str) -> bool {
+    let mut stack = Stack::new();
+    for c in bracket.chars() {
+        match c {
+            '(' | '{' | '[' => stack.push(c),
+            ')' => {
+                if stack.pop() != Some('(') {
+                    return false;
+                }
+            }
+            '}' => {
+                if stack.pop() != Some('{') {
+                    return false;
+                }
+            }
+            ']' => {
+                if stack.pop() != Some('[') {
+                    return false;
+                }
+            }
+            _ => {}
+        }
+    }
+    stack.is_empty()
+}
